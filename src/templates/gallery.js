87541ab9ede9b81e22582ac8335f7c83cd1488db
Stuart <stupop010@hotmail.co.uk>
2020-04-03 16:49:27 +0100

    lazy loading to stop srr error

--- src/templates/gallery.js
+++ src/templates/gallery.js
@@ -1,22 +1,32 @@
-import React from "react"
+import React, { lazy, Suspense } from "react"
 import { graphql } from "gatsby"
 
 import Layout from "../components/layout/layout"
 import SEO from "../components/seo"
 import Gallery from "../components/gallery/gallery"
 
+const ClientSideOnlyLazyGallery = React.lazy(() =>
+  import("../components/gallery/gallery")
+)
+
 const GalleryTemplate = ({ data, pageContext }) => {
+  const isSSR = typeof window === "undefined"
+
   const { numPages, currentPage } = pageContext
 
   const pictures = data.allContentfulAsset.edges
   return (
     <Layout>
       <SEO title="Gallery" />
-      <Gallery
-        pictures={pictures}
-        numPages={numPages}
-        currentPage={currentPage}
-      />
+      {!isSSR && (
+        <Suspense fallback={<div />}>
+          <ClientSideOnlyLazyGallery
+            pictures={pictures}
+            numPages={numPages}
+            currentPage={currentPage}
+          />
+        </Suspense>
+      )}
     </Layout>
   )
 }
@@ -37,4 +47,19 @@ export const ImageListQuery = graphql`
   }
 `
 
-export default GalleryTemplate
+// import React from "react"
+
+// const MyPage = () => {
+//   const isSSR = typeof window === "undefined"
+//   return (
+//     <>
+//       {!isSSR && (
+//         <React.Suspense fallback={<div />}>
+//           <ClientSideOnlyLazy />
+//         </React.Suspense>
+//       )}
+//     </>
+//   )
+// }
+
+// export default GalleryTemplate
